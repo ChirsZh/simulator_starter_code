039a4b2a3ea2468d0be2017575257076c0cc0a56
Brandon Perez <bmperez@andrew.cmu.edu>
2016-11-20 00:26:25 -0500

    Updated the addi test comments with the correct register numbers.
    The register reference for the application binary interface (ABI) names
    for the registers was outdated. Also, the x2 register was no longer
    used, because this is the stack pointer. Also updated `crt0.S` as well.

--- inputs/addi.s
+++ inputs/addi.s
@@ -7,11 +7,11 @@
     .text                       # Declare the code to be in the .text segment
     .global main                # Make main visible to the linker
 main:
-    addi x2, zero, 10           # x2 = 0 + 10
-    addi t0, zero, 5            # t0 (x12) = 0 + 5
-    addi t1, t0, 300            # t1 (x13) = t0 + 300
-    addi t2, zero, 500          # t2 (x14) = 0 + 500
-    addi t3, t2, 34             # t3 (x15) = t2 + 34
-    addi t3, t3, 45             # t3 (x15) = t3 + 45
+    addi t0, zero, 10           # t0 (x5) = 0 + 10
+    addi t1, zero, 5            # t1 (x6) = 0 + 5
+    addi t2, t0, 300            # t2 (x7) = t1 + 300
+    addi t3, zero, 500          # t3 (x28) = 0 + 500
+    addi t4, t3, 34             # t4 (x29) = t3 + 34
+    addi t4, t4, 45             # t4 (x29) = t4 + 45
 
     ret                         # Return to the startup code
