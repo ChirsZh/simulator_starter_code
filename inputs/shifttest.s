2eaec9bf8a6dc65818594d50f87bc8d10d167470
Brandon Perez <bmperez@andrew.cmu.edu>
2016-11-26 12:36:40 -0500

    Ported the shifttest to the RISC-V ISA.

--- inputs/shifttest.s
+++ inputs/shifttest.s
@@ -1,12 +1,20 @@
-        # Basic shift tests
-	.text
-main:   
-        ori $3, $zero, 65535
-        sll   $4, $3, 16
-        sll   $5, $3, 6
-        sra   $6, $4, 10
-        sra   $7, $5, 16
-        srl   $8, $4, 8
-        srl   $9, $5, 16
-        addiu $2, $zero, 0xa
-        syscall
+# shifttest.s
+#
+# Basic Shift Instructions Test
+#
+# This performs a basic test of the shift instructions with immediate values.
+
+    .text                       # Declare the code to be in the .text segment
+    .global main                # Make main visible to the linker
+main:
+    ori     x3, zero, -1043     # x3 = 0xfffffbed
+    slli    x4, x3, 16          # x4 = x3 << 16
+    slli    x5, x3, 6           # x5 = x3 << 6
+    srai    x6, x4, 10          # x6 = x4 >> 10 (arithmetic)
+    srai    x7, x5, 16          # x7 = x5 >> 16 (arithmetic)
+    srli    x8, x4, 8           # x8 = x4 >> 8 (logical)
+    srli    x9, x5, 16          # x9 = x5 >> 16 (logical)
+
+    addi    a0, zero, 0xa       # a0 (x10) = 0xa
+    ecall                       # Terminate the simulation by passing 0xa to
+                                # ecall in register a0 (x10).
