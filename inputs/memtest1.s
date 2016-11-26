89e1b9c5a86291b0fbcd54925f62086d0c1c8c6d
Brandon Perez <bmperez@andrew.cmu.edu>
2016-11-26 12:23:29 -0500

    Ported memtest1 to the RISC-V ISA.

--- inputs/memtest1.s
+++ inputs/memtest1.s
@@ -1,47 +1,57 @@
-        # Basic half word/full word
-	.text
+# memtest1.s
+#
+# Advanced Memory Test
+#
+# This performs a more complete test of the load and instructions, checking
+# that sub-word (e.g. byte and half-word) instructions work. This alos tests
+# that the processor is respecting the little-endian memory ordering mandated
+# by the RISC-V ISA.
+
+    .data                       # Declare items to be in the .data segment
+data:                           # Symbol representing the start of .data
+    .space 20                   # Allocate 16 bytes for the .data segment
+data_end:                       # Symbol representing the end of .data
+
+    .text                       # Declare the code to be in the .text segment
+    .global main                # Make main visible to the linker
 main:
-        #;;  Set a base address
-        lui    $3, 0x1000
+    addi    x5, zero, 0x7fe     # x5 = 0x7de
+    addi    x6, zero, 0x5ca     # x6 = 0x5ca
+    addi    x7, zero, 0x33f     # x7 = 0x33f
+    lui     x8, 0xd             # x8 = 0xd000
+    addi    x8, x8, -1282       # x8 = x8 + 0xfffffafe (x8 = 0xcafe)
 
-        addiu  $5, $zero, 0xcafe
-        addiu  $6, $zero, 0xfeca
-        addiu  $7, $zero, 0xbeef
-        addiu  $8, $zero, 0xefbe
-                
-        #;; Place a test pattern in memory
-        sb     $5, 0($3)
-        sb     $6, 1($3)
-        sb     $7, 6($3)
-        sb     $8, 7($3)
+    sb      x5, 0(gp)           # *(gp + 0) = x5
+    sb      x6, 1(gp)           # *(gp + 0) = x6
+    sb      x7, 6(gp)           # *(gp + 0) = x7
+    sb      x8, 7(gp)           # *(gp + 0) = x8
 
-        lbu     $9,  0($3)
-        lbu     $10, 1($3)
-        lb      $11, 6($3)
-        lb      $12, 7($3)
+    lbu     x9,  0(gp)          # x9 = zero_extend(*gp + 0)
+    lbu     x10, 1(gp)          # x10 = zero_extend(*gp + 0)
+    lb      x11, 6(gp)          # x11 = sign_extend(*gp + 0)
+    lb      x12, 7(gp)          # x12 = sign_extend(*gp + 0)
 
-        addiu  $3, $3, 4
-        sh     $5, 0($3)
-        sh     $6, 2($3)
-        sh     $7, 4($3)
-        sh     $8, 6($3)
+    addi    gp, gp, 4           # gp (x3) = gp + 4
+    sh      x5, 0(gp)           # *(gp + 0) = x5
+    sh      x6, 2(gp)           # *(gp + 2) = x6
+    sh      x7, 4(gp)           # *(gp + 4) = x7
+    sh      x8, 6(gp)           # *(gp + 6) = x8
 
-        lhu     $13,  0($3)
-        lhu     $14,  2($3)
-        lh     $15,  4($3)
-        lh     $16,  6($3)
-               
-        #;; Calculate a "checksum" for easy comparison
-        add    $17, $zero, $9
-        add    $17, $17, $10
-        add    $17, $17, $11
-        add    $17, $17, $12
-        add    $17, $17, $13
-        add    $17, $17, $14
-        add    $17, $17, $15
-        add    $17, $17, $16
-        
-        #;;  Quit out 
-        addiu $v0, $zero, 0xa
-        syscall
-        
+    lhu     x13,  0(gp)         # x13 = zero_extend(gp + 0)
+    lhu     x14,  2(gp)         # x14 = zero_extend(gp + 2)
+    lh      x15,  4(gp)         # x15 = zero_extend(gp + 4)
+    lh      x16,  6(gp)         # x16 = zero_extend(gp + 6)
+
+    # Calculate a checksum for easy comparison
+    add     x17, zero, x9       # x17 = x9
+    add     x17, x17, x10       # x17 = x17 + x10
+    add     x17, x17, x11       # x17 = x17 + x11
+    add     x17, x17, x12       # x17 = x17 + x12
+    add     x17, x17, x13       # x17 = x17 + x13
+    add     x17, x17, x14       # x17 = x17 + x14
+    add     x17, x17, x15       # x17 = x17 + x15
+    add     x17, x17, x16       # x17 = x17 + x16
+
+    addi    a0, zero, 0xa       # a0 (x10) = 0xa
+    ecall                       # Terminate the simulation by passing 0xa to
+                                # ecall in register a0 (x10).
