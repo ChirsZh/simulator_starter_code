c2f8bfb09fc956bdeb06686d4b415c85d2dc5abe
Brandon Perez <bmperez@andrew.cmu.edu>
2017-01-04 21:38:38 -0500

    Added in comments to tell which labels should be reached in the code.

--- 447inputs/brtest1.s
+++ 447inputs/brtest1.s
@@ -10,7 +10,7 @@
 main:
     addi    a0,   zero,  0xa        # a0 (x10) = 0xa
 
-label0:
+label0:                             # Should reach here (1.)
     addi    x5,   zero,  1          # x5 = 1
     jal     zero, label1            # Goto label1 (no link)
 
@@ -20,23 +20,23 @@ label0:
     addi    x5,   zero,  100        # x5 = 100
     ecall                           # Terminate simulation (should not reach here)
 
-label1:
+label1:                             # Should reach here (2.)
     bne     zero, zero,  label3     # If (0 != 0) goto label3
     ori     x0,   x0,    0          # No-op
     ori     x0,   x0,    0          # No-op
     addi    x6,   zero,  1337       # x6 = 1337
 
-label2:
+label2:                             # Should reach here (3.)
     beq     zero, zero,  label4     # If (0 == 0) goto label4
     ori     x0,   x0,    0          # No-op
     ori     x0,   x0,    0          # No-op
     addi    x7,   zero,  0x347      # x7 = 0x347
     ecall                           # Terminate simulation (should not reach here)
 
-label3:
+label3:                             # Should not reach here
     addi    x8,   zero,  0x404      # x8 = 0x3404
     ecall                           # Terminate simulation (should not reach here)
 
-label4:
+label4:                             # Should reach here
     addi    x7,   zero,  0x447      # x7 = 0x447
     ecall                           # Terminate simulation (should reach here)
