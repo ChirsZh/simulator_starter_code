420b779194e33606f6b7298eff3224d7386f420c
Brandon Perez <bmperez@andrew.cmu.edu>
2017-01-07 19:17:58 -0500

    Added in Professor Hoe's changes to brtest0.
    This was mainly updating it so that the x7 register was used as an
    accumulator.

--- 447inputs/brtest0.s
+++ 447inputs/brtest0.s
@@ -9,21 +9,23 @@
     .global main                    # Make main visible to the linker
 main:
     addi    a0,   zero,  0xa        # a0 (x10) = 0xa
+    addi    x7,   zero,  0          # x7 = 0x0
 
-label0:                             # Should reach here (1.)
+label0:
     jal     zero, label1            # Goto label1 (no link)
+    addi    x7,   x7,    0x327      # x7 = x7 + 0x327
 
-label1:                             # Should reach here (2.)
+label1:
     bne     zero, zero,  label3     # If (0 != 0) goto label3
 
-label2:                             # Should reach here (3.)
+label2:
     beq     zero, zero,  label4     # If (0 == 0) goto label4
-    addi    x7,   zero,  0x347      # x7 = 0x347
+    addi    x7,   x7,    0x347      # x7 = x7 + 0x347
     ecall                           # Terminate simulation (should not reach here)
 
-label3:                             # Should not reach here
-    addi    x6,   zero,  0x337      # x6 = 0x337
+label3:
+    addi    x7,   x7,    0x337       # x7 = x7 + 0x337
 
-label4:                             # Should reach here (4.)
-    addi    x7,   zero,  0x70d      # x7 = 0x70d
+label4:
+    addi    x7,   x7,    0x70d       # x7 = x7 + 0x70d
     ecall                           # Terminate simulation (should reach here)
