b668815ca7c0c3ab0999c3d560925c97b64b9ed0
Brandon Perez <bmperez@andrew.cmu.edu>
2017-04-30 19:43:35 -0400

    Cleaned up the fibonacci test.

--- 447inputs/fib.c
+++ 447inputs/fib.c
@@ -1,15 +1,24 @@
-int fib(int i);
+/**
+ * fib.c
+ *
+ * Fibonacci Test
+ *
+ * This test is the naive implementation of the Fibonacci function. This
+ * generally tests a good subset of the instructions.
+ **/
 
-int fib(int i) {
-  if (i==0) {
-    return 1;
-  } else if (i==1) {
-    return 1;
-  } else {
-    return fib(i-1)+fib(i-2);
-  }
+int fib(int i)
+{
+    if (i == 0) {
+        return 1;
+    } else if (i == 1) {
+        return 1;
+    } else {
+        return fib(i - 1) + fib(i - 2);
+    }
 }
 
-int main() {
-  return fib(10);
+int main()
+{
+    return fib(10);     // fib(10) = 89. This value will appear in x2 (sp).
 }
