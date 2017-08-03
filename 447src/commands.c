edec9484d9d46befdca34314478c1a07776e9377
Brandon Perez <bmperez@alumni.cmu.edu>
2017-08-02 23:01:30 -0700

    Moved the RISC-V register name definitions into their own file.
    This was done to cleanup the commands file, and remove the clutter from
    defining the array of all the register names for the registers in the
    RISC-V ISA.

--- 447src/commands.c
+++ 447src/commands.c
@@ -22,23 +22,24 @@
  *----------------------------------------------------------------------------*/
 
 // Standard Includes
-#include <stdlib.h>             // Malloc and related functions
-#include <stdio.h>              // Printf and related functions
-#include <stdbool.h>            // Definition of the boolean type
-#include <stdint.h>             // Fixed-size integral types
+#include <stdlib.h>                 // Malloc and related functions
+#include <stdio.h>                  // Printf and related functions
+#include <stdbool.h>                // Definition of the boolean type
+#include <stdint.h>                 // Fixed-size integral types
 
 // Standard Includes
-#include <limits.h>             // Limits for integer types
-#include <assert.h>             // Assert macro
-#include <errno.h>              // Error codes and perror
+#include <limits.h>                 // Limits for integer types
+#include <assert.h>                 // Assert macro
+#include <errno.h>                  // Error codes and perror
 
 // 18-447 Simulator Includes
-#include <sim.h>                // Definition of cpu_state_t
+#include <sim.h>                    // Definition of cpu_state_t
 
 // Local Includes
-#include "memory_shell.h"       // Interface to the processor memory
-#include "libc_extensions.h"    // Parsing functions, array_len, Snprintf
-#include "commands.h"           // This file's interface
+#include "memory_shell.h"           // Interface to the processor memory
+#include "libc_extensions.h"        // Parsing functions, array_len, Snprintf
+#include "riscv_register_names.h"   // Names for the RISC-V registers
+#include "commands.h"               // This file's interface
 
 /*----------------------------------------------------------------------------
  * Shared Helper Functions
@@ -220,48 +221,6 @@ void command_go(cpu_state_t *cpu_state, char *args[], int num_args)
                                         string_len("Uint Value"))
 #define REG_INT_COL_LEN         max(INT32_MAX_DIGITS + string_len("()") + 1, \
                                         string_len("Int Value"))
-
-// Structure representing the naming information about a register
-typedef struct register_name {
-    const char *isa_name;       // The ISA name for a register (x0..x31)
-    const char *abi_name;       // The ABI name for a register (sp, t0, etc.)
-} register_name_t;
-
-// An array of all the naming information for a register, and its ABI aliases
-static const register_name_t RISCV_REGISTER_NAMES[RISCV_NUM_REGS] = {
-    { .isa_name = "x0",  .abi_name = "zero", },
-    { .isa_name = "x1",  .abi_name = "ra", },
-    { .isa_name = "x2",  .abi_name = "sp", },
-    { .isa_name = "x3",  .abi_name = "gp", },
-    { .isa_name = "x4",  .abi_name = "tp", },
-    { .isa_name = "x5",  .abi_name = "t0", },
-    { .isa_name = "x6",  .abi_name = "t1", },
-    { .isa_name = "x7",  .abi_name = "t2", },
-    { .isa_name = "x8",  .abi_name = "s0/fp", },
-    { .isa_name = "x9",  .abi_name = "s1", },
-    { .isa_name = "x10", .abi_name = "a0", },
-    { .isa_name = "x11", .abi_name = "a1", },
-    { .isa_name = "x12", .abi_name = "a2", },
-    { .isa_name = "x13", .abi_name = "a3", },
-    { .isa_name = "x14", .abi_name = "a4", },
-    { .isa_name = "x15", .abi_name = "a5", },
-    { .isa_name = "x16", .abi_name = "a6", },
-    { .isa_name = "x17", .abi_name = "a7", },
-    { .isa_name = "x18", .abi_name = "s2", },
-    { .isa_name = "x19", .abi_name = "s3", },
-    { .isa_name = "x20", .abi_name = "s4", },
-    { .isa_name = "x21", .abi_name = "s5", },
-    { .isa_name = "x22", .abi_name = "s6", },
-    { .isa_name = "x23", .abi_name = "s7", },
-    { .isa_name = "x24", .abi_name = "s8", },
-    { .isa_name = "x25", .abi_name = "s9", },
-    { .isa_name = "x26", .abi_name = "s10", },
-    { .isa_name = "x27", .abi_name = "s11", },
-    { .isa_name = "x28", .abi_name = "t3", },
-    { .isa_name = "x29", .abi_name = "t4", },
-    { .isa_name = "x30", .abi_name = "t5", },
-    { .isa_name = "x31", .abi_name = "t6", },
-};
 
 /**
  * Tries to find the register with a matching ISA name or ABI alias from the
